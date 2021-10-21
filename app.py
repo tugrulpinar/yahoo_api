c78b6627fabcfffa7747f9e3b38572d128fb2c89
Tugrul Pinar <63513339+tugrulpinar@users.noreply.github.com>
2021-10-20 20:29:42 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import requests
+import os
+
+# Hide your API KEY
+KEY = os.environ["YAHOO_KEY"]
+
+
+class YahooApi:
+    def __init__(self):
+        self.url = "https://rest.yahoofinanceapi.com/v6/finance/quote"
+
+    def get_last_close_price(self, symbol: str) -> int:
+        """Gets the latest closing price"""
+
+        querystring = {"symbols": symbol}
+
+        headers = {
+            'x-api-key': KEY
+        }
+
+        try:
+            response = requests.get(
+                self.url, headers=headers, params=querystring)
+
+            res = response.json()
+
+            return res["quoteResponse"]["result"][0]["regularMarketPreviousClose"]
+
+        except Exception as e:
+            return e
+
+
+yahoo = YahooApi()
+print(yahoo.get_last_close_price("AAPL"))
