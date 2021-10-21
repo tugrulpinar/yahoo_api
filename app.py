795118862ebe8e09c35f117d883c3afa5e8140f7
Tugrul Pinar <63513339+tugrulpinar@users.noreply.github.com>
2021-10-21 16:00:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import requests
 import os
 
 # Hide your API KEY
-KEY = os.environ["YAHOO_KEY"]
+# KEY = os.environ["YAHOO_KEY"]
 
+KEY = "Uuu04gRjLsanSr1b9JUXp3PzTXY7kMjV8mIFlMq3"
 
 class YahooApi:
     def __init__(self):
