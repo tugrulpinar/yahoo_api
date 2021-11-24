29d59b9cd536950436cac43e3c53470c75a107ea
tugrulpinar <tugrulpinar@protonmail.com>
2021-11-23 21:02:18 -0500

    initial

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import requests
 import os
 
 # Hide your API KEY
-# KEY = os.environ["YAHOO_KEY"]
+KEY = os.environ["YAHOO_KEY"]
 
-KEY = "Uuu04gRjLsanSr1b9JUXp3PzTXY7kMjV8mIFlMq3"
+# KEY = "Uuu04gRjLsanSr1b9JUXp3PzTXY7kMjV8mIFlMq3"
+
 
 class YahooApi:
     def __init__(self):
